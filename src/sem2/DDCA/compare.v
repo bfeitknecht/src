cb3cfe930a207798b661378863b71f4f7f5049e7
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-07 18:24:37 +0100

    vault backup: 2024-03-07 18:24:37 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/compare.v
    src/sem2/DDCA/mux2.v

--- src/sem2/DDCA/compare.v
+++ src/sem2/DDCA/compare.v
@@ -1,9 +1,12 @@
 module compare (input [3:0]  a, input [3:0] b, output eq);
+
+    /*
     wire [3:0] c;
     assign c = ~(a ^ b);
     assign eq = &c;     // shorter than the previous version
-
-    // assign eq = (a == b) ? 1 : 0; // really short
+    */
+    
+    assign eq = (a == b) ? 1 : 0; // really short
 
 
 endmodule
