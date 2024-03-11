4e96f68c44080e04b1bafad81ba3fa57ea2ebf09
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-11 17:49:10 +0100

    vault backup: 2024-03-11 17:49:10 from MacBook Pro (echo)
    Affected files:
    src/sem2/Analysis-1/An1.lua

--- src/sem2/Analysis-1/An1.lua
+++ src/sem2/Analysis-1/An1.lua
@@ -1,11 +1,11 @@
 
 
 
-for k = 1, 1000 do
+for k = 1, 10000 do
 	if k % 3 == 1 then
-		a = 1 + math.sqrt(k / 12*k + 1)
+		a = 1 + math.sqrt(k /(12*k + 1))
 		elseif k % 3 == 2 then
-		a = 5*k^3 / k^3 + 1
+		a = 5*k^3 / (k^3 + 1)
 	elseif k % 3 == 0 then
 		a = (-1)^k / k
 	end
