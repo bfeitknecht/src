358be9d0360924ad0b3dedefd3df369538d078ca
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-15 14:24:38 +0100

    vault backup: 2024-03-15 14:24:38 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/FullAdder.v

--- src/sem2/DDCA/lab/lab02/FullAdder.v
+++ src/sem2/DDCA/lab/lab02/FullAdder.v
@@ -1,8 +1,8 @@
 module FullAdder (input a, input b, input ci, output s, output co);
-`include "HalfAdder.v"; 
+// `include "HalfAdder.v";
 wire y, c, x;
 HalfAdder ab(.a(a), .b(b), .s(y), .c(c));
 HalfAdder yci(.a(y), .b(ci), .s(s), .c(x));
-or (c, x, s);
+or carry (c, x, s);
 
 endmodule
