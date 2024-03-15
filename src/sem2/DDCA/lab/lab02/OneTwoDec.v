9ebdab5e4f1c9b272b5f3389319291d98bb26811
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-15 15:46:02 +0100

    vault backup: 2024-03-15 15:46:02 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/OneTwoDec.v
    src/sem2/DDCA/lab/lab02/TwoFourDec.v
    src/sem2/DDCA/lab/lab02/TwoFourDecode.v

---
+++ src/sem2/DDCA/lab/lab02/OneTwoDec.v
@@ -0,0 +1,9 @@
+module OneTwoDec (
+    input s,
+    output [1:0] o
+);
+    // wire ns;
+    // not NOTSEL(s, ns);
+    // assign o[0] = ns;
+    // assign o[1] = s;
+endmodule
---
+++ src/sem2/DDCA/lab/lab02/TwoFourDec.v
@@ -0,0 +1,7 @@
+module TwoFourDec (
+    input [1:0] s,
+    output [3:0] o
+);
+    
+    
+endmodule
