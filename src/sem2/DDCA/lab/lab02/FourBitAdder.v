e26c91049ce0edadea3b196f49232887088a6e11
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-15 14:59:38 +0100

    vault backup: 2024-03-15 14:59:38 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/FourBitAdder.v
    src/sem2/DDCA/lab/lab02/FullAdder.v
    src/sem2/DDCA/lab/lab02/HalfAdder.v

--- src/sem2/DDCA/lab/lab02/FourBitAdder.v
+++ src/sem2/DDCA/lab/lab02/FourBitAdder.v
@@ -4,11 +4,11 @@ input [3:0] b,
 output [4:0] s
 );
 
-wire c0, c1, c2;
+    wire c0, c1, c2;
 
-FullAdder fa0(.a(a[0]), .b(b[0]), .ci(0), .s(s[0]), .co(c0));
-FullAdder fa1(.a(a[1]), .b(b[1]), .ci(c0), .s(s[1]), .co(c1));
-FullAdder fa2(.a(a[2]), .b(b[2]), .ci(c1), .s(s[2]), .co(c0));
-FullAdder fa3(.a(a[3]), .b(b[3]), .ci(c2), .s(s[3]), .co(s[4]));
+    FullAdder FA0(.a(a[0]), .b(b[0]), .ci(0), .s(s[0]), .co(c0));
+    FullAdder FA1(.a(a[1]), .b(b[1]), .ci(c0), .s(s[1]), .co(c1));
+    FullAdder FA2(.a(a[2]), .b(b[2]), .ci(c1), .s(s[2]), .co(c0));
+    FullAdder FA3(.a(a[3]), .b(b[3]), .ci(c2), .s(s[3]), .co(s[4]));
 
 endmodule
--- src/sem2/DDCA/lab/lab02/FullAdder.v
+++ src/sem2/DDCA/lab/lab02/FullAdder.v
@@ -1,6 +1,8 @@
 module FullAdder (input a, input b, input ci, output s, output co);s
-wire abs, abc, abscic;
-HalfAdder ab(.a(a), .b(b), .s(abs), .c(abc));
-HalfAdder absci(.a(abs), .b(ci), .s(s), .c(abscic));
-or carry (abc, abscic, co);
+    
+    wire abs, abc, abscic;
+    HalfAdder HF0(.a(a), .b(b), .s(abs), .c(abc));
+    HalfAdder HF1(.a(abs), .b(ci), .s(s), .c(abscic));
+    or CARRY (abc, abscic, co);
+
 endmodule
