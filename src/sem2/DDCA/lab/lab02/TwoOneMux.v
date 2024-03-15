66bdf243df22371cfd0371748a92cbd27e4cecbb
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-15 15:14:38 +0100

    vault backup: 2024-03-15 15:14:38 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/TwoOneMux.v

--- src/sem2/DDCA/lab/lab02/TwoOneMux.v
+++ src/sem2/DDCA/lab/lab02/TwoOneMux.v
@@ -1,8 +1,13 @@
 module TwoOneMux(
     input a,
     input b,
-    input sel,
+    input s,
     output o
 );
+    wire sA, sB, nS;
+    not nS(s, nS);
+    and A(a, s, sA);
+    and B(b, nS, sB);
+    or OUT(sA, sB, o);
 
 endmodule
