1b500d9b060a67673d4931979b90af34a2f37d46
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-15 13:34:38 +0100

    vault backup: 2024-03-15 13:34:38 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/FullAdder.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab02/FullAdder.v
+++ src/sem2/DDCA/lab/lab02/FullAdder.v
@@ -1,11 +1,10 @@
 module FullAdder (input a, input b, input ci, output s, output co);
 
-wire abc;
-wire abs;
-wire abcic;
-HalfAdder ab(.a(a), .b(b), .s(abs) .c(abc))
-HalfAdder ab(.a(abs), .b(ci), .s(s), .c(abs))
-
-or (abc, abcic, co)
+wire abc
+wire abs
+wire abcic
+HalfAdder ab (.a(a), .b(b), .s(abs) .c(abc))
+HalfAdder ab (.a(abs), .b(ci), .s(s), .c(abs))
+or carryout (abc, abcic, co)
 
 endmodule
