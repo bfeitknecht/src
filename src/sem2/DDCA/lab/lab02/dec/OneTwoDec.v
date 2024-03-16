349868d871b287a4558d8663fa44c8236dd83893
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-16 13:04:01 +0100

    vault backup: 2024-03-16 13:04:01 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02.zip
    src/sem2/DDCA/lab/lab02/dec/OneTwoDec.v

--- src/sem2/DDCA/lab/lab02/dec/OneTwoDec.v
+++ src/sem2/DDCA/lab/lab02/dec/OneTwoDec.v
@@ -1,8 +1,7 @@
-module OneTwoDec (
-    input s,
-    output [1:0] o
-);
-
-    o[0] = 0;
-    s == 1 ? o[1] = 1 : o[1] = 0;
-endmodule
+// module OneTwoDec (
+//     input s,
+//     output [1:0] o
+// );
+//     o[0] = 0;
+//     s == 1 ? o[1] = 1 : o[1] = 0;
+// endmodule
