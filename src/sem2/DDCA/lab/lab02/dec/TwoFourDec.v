c0e8ff75f7f08b46cbfe7ff290e8bd35aceb60f8
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-16 12:59:01 +0100

    vault backup: 2024-03-16 12:59:01 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v

--- src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
+++ src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
@@ -2,6 +2,15 @@ module TwoFourDec (
     input [1:0] s,
     output [3:0] o
 );
+
+    wire ns0, ns1;
+
+    not NOTSEL0(s[0], ns0);
+    not NOTSEL1(s[1], ns1);
     
+    and D0(ns0, ns1, o[0]);
+    and D1(s[0], ns1, o[1]);
+    and D2(ns0, s[1], o[2]);
+    and D3(s[0], s[1], o[3]);
     
 endmodule
