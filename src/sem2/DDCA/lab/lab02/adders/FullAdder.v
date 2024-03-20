24dfd28d03434b8e7d2590d70132e1d039647ae0
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-20 14:25:18 +0100

    vault backup: 2024-03-20 14:25:18 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/adders/FullAdder.v

--- src/sem2/DDCA/lab/lab02/adders/FullAdder.v
+++ src/sem2/DDCA/lab/lab02/adders/FullAdder.v
@@ -1,11 +1,5 @@
 module FullAdder (input a, input b, input ci, output s, output co);s
     
-    // module instantiation method 
-    // wire s0, c0, c1;
-    // HalfAdder HA0(.a(a), .b(b), .s(s0), .c(c0));
-    // HalfAdder HA1(.a(s0), .b(ci), .s(s), .c(c1));
-    // or CARRY (c0, c1, co);
-
     wire s0, c0;
 
     xor s0 (a, b, s0);
@@ -23,3 +17,9 @@ endmodule
     S = a XOR b XOR ci
     CO = a AND b OR (a XOR b) AND ci
 */
+
+// module instantiation method 
+// wire s0, c0, c1;
+// HalfAdder HA0(.a(a), .b(b), .s(s0), .c(c0));
+// HalfAdder HA1(.a(s0), .b(ci), .s(s), .c(c1));
+// or CARRY (c0, c1, co);
