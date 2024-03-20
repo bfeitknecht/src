c0e12a4cd0ea2b19293a806ad36da7dab755737c
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-20 20:39:21 +0100

    vault backup: 2024-03-20 20:39:21 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab03/top/Top.v

--- src/sem2/DDCA/lab/lab03/top/Top.v
+++ src/sem2/DDCA/lab/lab03/top/Top.v
@@ -2,7 +2,6 @@ module TwoFourDec (
     input [1:0] s,
     output [3:0] o
 );
-
     wire ns0, ns1;
     not NOTSEL0 (s[0], ns0);
     not NOTSEL1 (s[1], ns1);
@@ -11,7 +10,6 @@ module TwoFourDec (
     and OUT1 (s[0], ns1, o[1]);
     and OUT2 (ns0, s[1], o[2]);
     and OUT3 (s[0], s[1], o[3]);
-    
 endmodule
 
 module Top(
@@ -19,11 +17,9 @@ module Top(
     output [3:0] AN
 );
     wire [3:0] bus;
-
+    
     // 2:4 DEC, input s[1:0], output not AN[3:0]
     TwoFourDec SEL (.s(s[1:0]), .o(bus[3:0]));
 
     assign AN = ~bus;
-
-
 endmodule
