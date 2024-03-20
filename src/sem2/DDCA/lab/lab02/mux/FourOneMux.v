e18389cbb43b4f49ba2349d2665194ddf4f4313c
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-20 15:25:18 +0100

    vault backup: 2024-03-20 15:25:18 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/mux/FourOneMux.v
    src/sem2/DDCA/lab/lab02/mux/TwoOneMux.v

--- src/sem2/DDCA/lab/lab02/mux/FourOneMux.v
+++ src/sem2/DDCA/lab/lab02/mux/FourOneMux.v
@@ -1,3 +1,17 @@
+module TwoOneMux(
+    input d0,
+    input d1,
+    input s,
+    output o
+);
+    wire d0s, d1s, ns;
+    not NOTSEL(ns, s);
+    
+    and d0sel(d0s, d0, ns);
+    and d1sel(d1s, d1, s);
+    or OUT(o, d0s, d1s);
+endmodule
+
 module FourOneMux(
     input [3:0] d,
     input [1:0] s,
--- src/sem2/DDCA/lab/lab02/mux/TwoOneMux.v
+++ src/sem2/DDCA/lab/lab02/mux/TwoOneMux.v
@@ -5,10 +5,10 @@ module TwoOneMux(
     output o
 );
     wire d0s, d1s, ns;
-    not NOTSEL(s, ns);
-    
-    and d0(d0, ns, d0s);
-    and d1(d1, s, d1s);
-    or OUT(d0s, d1s, o);
+    not NOTSEL(ns, s);
+
+    and d0sel(d0s, d0, ns);
+    and d1sel(d1s, d1, s);
+    or OUT(o, d0s, d1s);
 
 endmodule
