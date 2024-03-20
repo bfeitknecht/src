f6f05f200dd89136a48abef2ea9e0d4634333a8c
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-20 14:20:18 +0100

    vault backup: 2024-03-20 14:20:18 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/adders/FullAdder.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab02/adders/FullAdder.v
+++ src/sem2/DDCA/lab/lab02/adders/FullAdder.v
@@ -1,19 +1,19 @@
 module FullAdder (input a, input b, input ci, output s, output co);s
     
-    wire s0, c0, c1;
+    // module instantiation method 
+    // wire s0, c0, c1;
+    // HalfAdder HA0(.a(a), .b(b), .s(s0), .c(c0));
+    // HalfAdder HA1(.a(s0), .b(ci), .s(s), .c(c1));
+    // or CARRY (c0, c1, co);
 
-    HalfAdder HA0(.a(a), .b(b), .s(s0), .c(c0));
-    HalfAdder HA1(.a(s0), .b(ci), .s(s), .c(c1));
-    or CARRY (c0, c1, co);
-
-    wire s0, c0, c1;
+    wire s0, c0;
 
     xor s0 (a, b, s0);
     xor SUM (s0, ci, s);
 
     and c0 (a, b, s0);
-    and c1 (s0, ci, c1);
-    or CARRY (c1, ci, co);
+    and c1 (s0, ci, c0);
+    or CARRY (c0, ci, co);
 
 endmodule
 
