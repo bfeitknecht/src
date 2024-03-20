fd67dddfa3f7785c7f18aedd8ca27f24edd3a30a
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-20 20:19:21 +0100

    vault backup: 2024-03-20 20:19:20 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab03/top/Top.v
    src/sem2/DDCA/lab/lab03/xdc/Top.xdc

--- src/sem2/DDCA/lab/lab03/top/Top.v
+++ src/sem2/DDCA/lab/lab03/top/Top.v
@@ -1,7 +1,25 @@
+module TwoFourDec (
+    input [1:0] s,
+    output [3:0] o
+);
+
+    wire ns0, ns1;
+    not NOTSEL0 (s[0], ns0);
+    not NOTSEL1 (s[1], ns1);
+
+    and OUT0 (ns0, ns1, o[0]);
+    and OUT1 (s[0], ns1, o[1]);
+    and OUT2 (ns0, s[1], o[2]);
+    and OUT3 (s[0], s[1], o[3]);
+    
+endmodule
+
 module Top(
     input [1:0] s,
     output [3:0] AN
 );
 
+    TwoFourDec SEL (.o(AN), .s(s));
+
+
 endmodule
-```
---
+++ src/sem2/DDCA/lab/lab03/xdc/Top.xdc
@@ -0,0 +1,8 @@
+
+# output AN[3:0] 
+set_property PACKAGE_PIN U2 [get_ports {AN[0]}]
+set_property PACKAGE_PIN U4 [get_ports {AN[1]}]
+set_property PACKAGE_PIN V4 [get_ports {AN[2]}]
+set_property PACKAGE_PIN W4 [get_ports {AN[3]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {AN}]
