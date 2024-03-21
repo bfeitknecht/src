01259457f3644334cbe21c4aacdb11fedab165db
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-21 08:10:03 +0100

    vault backup: 2024-03-21 08:10:03 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/add/FourBitAdder.v
    src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
    src/sem2/DDCA/lab/lab03/top/Sel7Seg.v
    src/sem2/DDCA/lab/lab03/xdc/Sel7Seg.xdc

--- src/sem2/DDCA/lab/lab02/add/FourBitAdder.v
+++ src/sem2/DDCA/lab/lab02/add/FourBitAdder.v
@@ -8,20 +8,18 @@ module FullAdder (input a, input b, input ci, output s, output co);
     and c0 (co0, a, b);
     and c1 (co1, s0, ci);
     or CARRY (co, c0, c1);
-
 endmodule
 
-module FourBitAdder(
-    input [3:0] a,
-    input [3:0] b,
-    output [4:0] s
-);
 
+module FourBitAdder(
+    input wire [3:0] a,
+    input wire [3:0] b,
+    output wire [4:0] s
+);
     wire c0, c1, c2;
 
     FullAdder FA0(.a(a[0]), .b(b[0]), .ci(), .s(s[0]), .co(c0));
     FullAdder FA1(.a(a[1]), .b(b[1]), .ci(c0), .s(s[1]), .co(c1));
     FullAdder FA2(.a(a[2]), .b(b[2]), .ci(c1), .s(s[2]), .co(c0));
     FullAdder FA3(.a(a[3]), .b(b[3]), .ci(c2), .s(s[3]), .co(s[4]));
-
 endmodule
--- src/sem2/DDCA/lab/lab03/top/Sel7Seg.v
+++ src/sem2/DDCA/lab/lab03/top/Sel7Seg.v
@@ -4,7 +4,7 @@ module TwoFourDec (
 );
     wire ns0, ns1;
     not NOTSEL0 (ns0, s[0]);
-    not NOTSEL1 (ns1, (s[1]));
+    not NOTSEL1 (ns1, s[1]);
 
     and OUT0 (o[0], ns0, ns1);
     and OUT1 (o[1], s[0], ns1);
@@ -12,20 +12,18 @@ module TwoFourDec (
     and OUT3 (o[3], s[0], s[1]);
 endmodule
 
-module Select7Seg(
+module Sel7Seg(
     input wire [1:0] s,
     output wire [3:0] AN
 );
     wire [1:0] sel;
-    wire [3:0] bus;
+    wire [3:0] aus;
 
-    // assign sel = {s[1], s[0]};
-    
-    // 2:4 DEC, input s[1:0], output not AN[3:0]
+    // 2:4 DEC, input s[1:0], output NOT AN[3:0]
     TwoFourDec SEL (
         .s(s[1:0]),
-        .o(bus[3:0])
+        .o(aus[3:0])
     );
 
-    assign AN = ~bus;
+    assign AN = ~aus;
 endmodule
