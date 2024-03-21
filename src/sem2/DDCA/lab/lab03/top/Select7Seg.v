ffee1586b8c058afc006f65890ad599d1769dfe5
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-21 08:05:03 +0100

    vault backup: 2024-03-21 08:05:03 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab03/top/Select7Seg.v

--- src/sem2/DDCA/lab/lab03/top/Select7Seg.v
+++ src/sem2/DDCA/lab/lab03/top/Select7Seg.v
@@ -3,16 +3,16 @@ module TwoFourDec (
     output wire [3:0] o
 );
     wire ns0, ns1;
-    not NOTSEL0 (s[0], ns0);
-    not NOTSEL1 (s[1], ns1);
+    not NOTSEL0 (ns0, s[0]);
+    not NOTSEL1 (ns1, (s[1]));
 
-    and OUT0 (ns0, ns1, o[0]);
-    and OUT1 (s[0], ns1, o[1]);
-    and OUT2 (ns0, s[1], o[2]);
-    and OUT3 (s[0], s[1], o[3]);
+    and OUT0 (o[0], ns0, ns1);
+    and OUT1 (o[1], s[0], ns1);
+    and OUT2 (o[2], ns0, s[1]);
+    and OUT3 (o[3], s[0], s[1]);
 endmodule
 
-module Top(
+module Select7Seg(
     input wire [1:0] s,
     output wire [3:0] AN
 );
