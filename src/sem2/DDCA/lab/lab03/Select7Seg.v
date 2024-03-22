26833255cae0dc1440f93155609ef949d812eb66
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-22 10:35:47 +0100

    vault backup: 2024-03-22 10:35:47 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab03/Select7Seg.v

--- src/sem2/DDCA/lab/lab03/Select7Seg.v
+++ src/sem2/DDCA/lab/lab03/Select7Seg.v
@@ -1,14 +1,14 @@
 module TwoFourDec (
     input wire [1:0] s,
-    output wire [3:0] o);
+    output wire [3:0] Y);
     wire ns0, ns1;
     not NOTSEL0 (ns0, s[0]);
     not NOTSEL1 (ns1, s[1]);
 
-    and OUT0 (o[0], ns0, ns1);
-    and OUT1 (o[1], s[0], ns1);
-    and OUT2 (o[2], ns0, s[1]);
-    and OUT3 (o[3], s[0], s[1]);
+    and OUT0 (Y[0], ns0, ns1);
+    and OUT1 (Y[1], s[0], ns1);
+    and OUT2 (Y[2], ns0, s[1]);
+    and OUT3 (Y[3], s[0], s[1]);
 endmodule
 
 module Select7Seg(
@@ -20,7 +20,7 @@ module Select7Seg(
     // 2:4 DEC, input s[1:0], output INV AN[3:0]
     TwoFourDec SEL (
         .s(s[1:0]),
-        .o(INVSEL[3:0])
+        .Y(INVSEL[3:0])
     );
 
     assign AN = ~INVSEL;
