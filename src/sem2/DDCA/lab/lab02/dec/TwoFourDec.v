e3562293f47cf39ec388c46b918f1093e5d9e706
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-22 10:17:18 +0100

    vault backup: 2024-03-22 10:17:18 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
+++ src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
@@ -1,13 +1,13 @@
 module TwoFourDec (
     input wire [1:0] s,
-    output wire [3:0] o
-);
+    output wire [3:0] o);
+
     wire [1:0] ns;
-    
     not NOTSEL0 (ns[0], s[0]);
     not NOTSEL1 (ns[1], s[1]);
-    and OUT0 (o[0], ns[0], ns1);
-    and OUT1 (o[1], s[0], ns1);
-    and OUT2 (o[2], ns0, s[1]);
+    
+    and OUT0 (o[0], ns[0], ns[1]);
+    and OUT1 (o[1], s[0], ns[1]);
+    and OUT2 (o[2], ns[0], s[1]);
     and OUT3 (o[3], s[0], s[1]);
 endmodule
