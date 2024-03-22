6a85f5ebb66fc790647bd9025edb5b31e192e42a
bfeitknecht <bfeitknecht@ethz.ch>
2024-03-22 10:12:18 +0100

    vault backup: 2024-03-22 10:12:18 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab02/dec/TwoFourDec.v
    src/sem2/DDCA/var/mux2.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/var/mux2.v
+++ src/sem2/DDCA/var/mux2.v
@@ -1,9 +1,7 @@
-module mux2 
-    #(parameter width = 8)
-    (input [width-1:0] d0, d1,
+module mux2 #(parameter width = 8) (
     input s,
+    input [width-1:0] d0, d1,
     output [width-1:0] y);
 
     assign y = s ? d1 : d0;
-
 endmodule
