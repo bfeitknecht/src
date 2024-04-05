823e374a5f590f7e425f4c703c44cdf328ead8ca
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-05 17:20:40 +0200

    vault backup: 2024-04-05 17:20:40 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab03/src/BinaryToSelect7Seg.v
    src/sem2/DDCA/lab/lab03/xdc/BinaryToSelect7Seg.xdc

--- src/sem2/DDCA/lab/lab03/src/BinaryToSelect7Seg.v
+++ src/sem2/DDCA/lab/lab03/src/BinaryToSelect7Seg.v
@@ -49,9 +49,39 @@ module Drive7Seg(
     end
 endmodule
 
+module TwoFourDec (
+    input [1:0] s,
+    output [3:0] Y);
+
+    wire [1:0] ns;
+    not ns0 (ns[0], s[0]);
+    not ns1 (ns[1], s[1]);
+
+    and Y0 (Y[0], ns[0], ns[1]);
+    and Y1 (Y[1], s[0], ns[1]);
+    and Y2 (Y[2], ns[0], s[1]);
+    and Y3 (Y[3], s[0], s[1]);
+endmodule
+
+module Select7Seg(
+    input wire [1:0] s,
+    output wire [3:0] AN);
+
+    wire [3:0] INVSEL;
+    
+    // 2:4 DEC, input s[1:0], output INV AN[3:0]
+    TwoFourDec NOTAN (
+        .s(s[1:0]),
+        .Y(INVSEL[3:0])
+    );
+
+    assign AN = ~INVSEL;
+endmodule
+
 module BinaryToSelect7Seg(
     input [3:0] a,
     input [3:0] b,
+    input [1:0] s,
     output [3:0] AN,
     output [6:0] DISPLAY,
     output OVERFLOW);
@@ -67,10 +97,12 @@ module BinaryToSelect7Seg(
         .S(S[3:0]),
         .D(DISPLAY[6:0])
     );
+    Select7Seg SELECT (
+        .s(s[1:0]),
+        .AN(AN)
+    );
     
     assign OVERFLOW = S[4];
-    assign AN[3:1] = 1;
-    assign AN[0] = 0;
 endmodule
 
 /*
