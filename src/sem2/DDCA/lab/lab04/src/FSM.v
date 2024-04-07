1a512dde1d5c4e9dfde4387101691f40a6244395
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-07 22:14:04 +0200

    vault backup: 2024-04-07 22:14:04 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/FSM.v
    vault/.obsidian/plugins/obsidian-git/data.json

--- src/sem2/DDCA/lab/lab04/src/FSM.v
+++ src/sem2/DDCA/lab/lab04/src/FSM.v
@@ -1,10 +1,10 @@
 module clk_div(input clk, input rst, output clk_en);
-reg [24:0] clk_count;
-always @ (posedge clk) begin
-    if (rst) clk_count <= 0;
-    else clk_count <= clk_count + 1;
-end
-assign clk_en = &clk_count;
+    reg [24:0] clk_count;
+    always @ (posedge clk) begin
+        if (rst) clk_count <= 0;
+        else clk_count <= clk_count + 1;
+    end
+    assign clk_en = &clk_count;
 endmodule
 
 module FSM(
@@ -16,12 +16,21 @@ module FSM(
 
     integer state;
     reg [2:0] pattern;
+    wire clk_en;
 
-    always @ (posedge clk) begin
+    clk_div CLKDIV (
+        .clk(clk),
+        .rst(rst),
+        .clk_en(clk_en)
+    );
+
+    always @ (posedge clk_en) begin
         if (rst) state <= 0;
         else state <= state++ % 4;
 
         pattern <= (1 << state) - 1;
+
+        
 
         // non blocking, so both can happen at the same time
         // assign {LA, LB, LC} = (l) ? pattern[2:0] : 3'b000;
@@ -46,7 +55,4 @@ pattern = (1 << state) - 1
     0110
     0111    *
     1000
-
-    assign {LA, LB, LC} = (l) ? pattern[2:0] : 3'b000;
-    assign {RA, RB, RC} = (r) ? pattern[0:2] : 3'b000;
 */
