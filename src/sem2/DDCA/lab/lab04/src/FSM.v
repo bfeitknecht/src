2dc18d360577e84e183a662f0dfa9a7586092b22
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-07 22:04:12 +0200

    vault backup: 2024-04-07 22:04:12 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/FSM.v

--- src/sem2/DDCA/lab/lab04/src/FSM.v
+++ src/sem2/DDCA/lab/lab04/src/FSM.v
@@ -10,25 +10,43 @@ endmodule
 module FSM(
     input clk,
     input rst,
-    input L,
-    input R,
+    input l,
+    input r,
     output LA, LB, LC, RA, RB,RC);
 
     integer state;
+    reg [2:0] pattern;
 
     always @ (posedge clk) begin
-        if (rst) state <= 3'b000;
-        else state <= (state++) % 4;
-    end
+        if (rst) state <= 0;
+        else state <= state++ % 4;
 
+        pattern <= (1 << state) - 1;
+
+        // non blocking, so both can happen at the same time
+        // assign {LA, LB, LC} = (l) ? pattern[2:0] : 3'b000;
+        // assign {RA, RB, RC} = (r) ? pattern[0:2] : 3'b000;
+    end
 endmodule
 
 /*
 state diagram:
-    0: 000 -> 000
+    0 -> 000   // 0
+    1 -> 001   // 1
+    2 -> 011   // 3
+    3 -> 111   // 7
 
-    1: 001 -> 001
-    2: 010 -> 011
-    3: 011 -> 111
-    4: 100 -> 000
+pattern = (1 << state) - 1
+    0000    *
+    0001    *
+    0010
+    0011    *
+    0100
+    0101
+    0110
+    0111    *
+    1000
+
+    assign {LA, LB, LC} = (l) ? pattern[2:0] : 3'b000;
+    assign {RA, RB, RC} = (r) ? pattern[0:2] : 3'b000;
 */
