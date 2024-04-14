a2ba1b6deb67d001836d19fb9e2b05a0bf77394c
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-14 16:50:01 +0200

    vault backup: 2024-04-14 16:50:01 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/FORD.v
    src/sem2/DDCA/lab/lab04/src/FSM.v

--- src/sem2/DDCA/lab/lab04/src/FORD.v
+++ src/sem2/DDCA/lab/lab04/src/FORD.v
@@ -12,6 +12,41 @@ module ClockDivisor #(parameter ratio = 1) (
     assign clk_out = &clk_count;
 endmodule
 
+module FSM (
+    input clk,
+    input reset,
+    input enable,
+    output L, M, R
+    );
+
+    // state holding registers
+    reg [2:0] state_p, state_n;
+    reg [2:0] S0 = 3'b000;
+    reg [2:0] S1 = 3'b001;
+    reg [2:0] S2 = 3'b011;
+    reg [2:0] S3 = 3'b111;
+    
+    // update state_ps based on inputs 
+    always @ (posedge enable) begin
+        case (state_p)
+            S0: state_n <= S1;
+            S1: state_n <= S2;
+            S2: state_n <= S3;
+            S3: state_n <= S0;
+            default: state_n = S0;
+        endcase
+    end
+
+
+    // next state_p logic
+    always @ (posedge clk, posedge reset) begin
+        if (reset) state_p <= S0;
+        else state_p <= state_n;
+    end
+
+    // output logic
+    assign {L, M, R} = state_n;
+endmodule
 
 module FORD(
     input clk_sys,
@@ -34,7 +69,7 @@ module FORD(
     FSM LEFT (
         .clk(clk),
         .reset(reset),
-        .selection(left),
+        .enable(left),
         .L(LC),
         .M(LB),
         .R(LA)
@@ -44,7 +79,7 @@ module FORD(
     FSM RIGHT (
     .clk(clk),
     .reset(reset),
-    .selection(right),
+    .enable(right),
     .L(RA),
     .M(RB),
     .R(RC)
--- src/sem2/DDCA/lab/lab04/src/FSM.v
+++ src/sem2/DDCA/lab/lab04/src/FSM.v
@@ -6,63 +6,32 @@ module FSM (
     );
 
     // state holding registers
-    reg [2:0] state_left, state_right, nextstate;
-    reg [1:0] selection;
-    
-    // update states based on inputs 
-    always @ (posedge enable) begin
-        
-    end
-
+    reg [2:0] state_p, state_n;
     reg [2:0] S0 = 3'b000;
     reg [2:0] S1 = 3'b001;
     reg [2:0] S2 = 3'b011;
     reg [2:0] S3 = 3'b111;
+    
+    // update state_ps based on inputs 
+    always @ (posedge enable) begin
+        case (state_p)
+            S0: state_n <= S1;
+            S1: state_n <= S2;
+            S2: state_n <= S3;
+            S3: state_n <= S0;
+            default: state_n = S0;
+        endcase
+    end
 
-    // next state logic
+
+    // next state_p logic
     always @ (posedge clk, posedge reset) begin
-
-        if (selection[1]) begin
-            case (state_left)
-                S0: nextstate <= S1;
-                S1: nextstate <= S2;
-                S2: nextstate <= S3;
-                S3: nextstate <= S0;
-            endcase
-        end
-        if (selection[0]) begin
-            case (state_right)
-                S0: nextstate <= S1;
-                S1: nextstate <= S2;
-                S2: nextstate <= S3;
-                S3: nextstate <= S0;
-            endcase
-        end
-
-
-        // selection[1] <= left;
-        // selection[0] <= right;
-
-
-        // if (reset) begin
-        //     state_left <= 0;
-        //     state_right <= 0;
-        //     selection  <= 0;
-        // end
-        // else begin
-        //     if (selection[1]) begin
-        //         if (~state_left) selection[1] <= 0;
-        //         state_left <= state_left + 1;
-        //     end
-        //     if (selection[0]) begin
-        //         if (~state_right) selection[0] <= 0;
-        //         state_right <= state_right + 1;
-        //     end
-        // end
+        if (reset) state_p <= S0;
+        else state_p <= state_n;
     end
 
     // output logic
-    assign {L, M, R} = nextstate;
+    assign {L, M, R} = state_n;
 endmodule
 
 
