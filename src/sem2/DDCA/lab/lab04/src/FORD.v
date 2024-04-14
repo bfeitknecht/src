8c9e54a02589cb47cc9ab9d435fa6127ced44f72
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-14 16:40:01 +0200

    vault backup: 2024-04-14 16:40:01 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/FORD.v

--- src/sem2/DDCA/lab/lab04/src/FORD.v
+++ src/sem2/DDCA/lab/lab04/src/FORD.v
@@ -1,5 +1,38 @@
 module FORD(
+    input clk_sys,
+    input left,
+    input right,
+    input reset,
+    output LA, LB, LC,
+    output RA, RB, RC
+    );
 
-);
+    // clock divisor
+    wire clk;
+    ClockDivisor #(.ratio(32)) clk_div (
+        .reset(reset),
+        .clk_in(clk_sys),
+        .clk_out(clk)
+        );
+
+    // left state machine
+    FSM LEFT (
+        .clk(clk),
+        .reset(reset),
+        .selection(left),
+        .L(LC),
+        .M(LB),
+        .R(LA)
+        );
+
+    // right state machine
+    FSM RIGHT (
+    .clk(clk),
+    .reset(reset),
+    .selection(right),
+    .L(RA),
+    .M(RB),
+    .R(RC)
+    );
 
 endmodule
