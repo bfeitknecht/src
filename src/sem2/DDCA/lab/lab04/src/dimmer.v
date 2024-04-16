913d85c6a4287e61a91f60e5861b5d69bc019dbc
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-16 16:03:30 +0200

    vault backup: 2024-04-16 16:03:30 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/dimmer.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab04/src/dimmer.v
+++ src/sem2/DDCA/lab/lab04/src/dimmer.v
@@ -28,8 +28,30 @@ module Dimmer (
 
     always @ (posedge clk_sys) begin
         // Dimmer logic
-
     end
+
+
+    reg [7:0] brightness;
+    reg [7:0] counter;
+
+    always @ (posedge clk) begin
+        if (reset) begin
+            brightness <= 0;
+            counter <= 0;
+        end else begin
+            if (counter < 255) begin
+                counter <= counter + 1;
+            end
+            else begin
+                counter <= 0;
+                if (brightness < 255) begin
+                    brightness <= brightness + 1;
+                end
+            end
+        end
+    end
+
+    assign D = (brightness >= counter);
 
 
 endmodule
