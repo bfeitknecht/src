65a2172a12f6e52522605c68b8e96e4851154f65
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-16 09:57:22 +0200

    vault backup: 2024-04-16 09:57:22 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src/FSM.v
    vault/.obsidian/workspace.json
    vault/.sem1.md

--- src/sem2/DDCA/lab/lab04/src/FSM.v
+++ src/sem2/DDCA/lab/lab04/src/FSM.v
@@ -4,31 +4,31 @@ module FSM (
     input enable,
     output L, M, R
     );
-    
+
     // state holding registers
-    reg [2:0] state_p, state_n;
+    reg [2:0] state_prev, state_next;
     parameter S0 = 3'b000;
     parameter S1 = 3'b001;
     parameter S2 = 3'b011;
     parameter S3 = 3'b111;
     
     // update state based on inputs 
-    always @ (posedge state_p) begin
-        case (state_p)
-            S0: state_n <= (enable) ? S1 : S0;
-            S1: state_n <= S2;
-            S2: state_n <= S3;
-            S3: state_n <= S0;
+    always @ (posedge state_prev) begin
+        case (state_prev)
+            S0: state_next <= (enable) ? S1 : S0;
+            S1: state_next <= S2;
+            S2: state_next <= S3;
+            S3: state_next <= S0;
         endcase
     end
 
 
-    // next state_p logic
+    // next state_prev logic
     always @ (posedge clk, posedge reset) begin
-        if (reset) state_p <= S0;
-        else state_p <= state_n;
+        if (reset) state_prev <= S0;
+        else state_prev <= state_next;
     end
 
     // output logic
-    assign {L, M, R} = state_n;
+    assign {L, M, R} = state_next;
 endmodule
