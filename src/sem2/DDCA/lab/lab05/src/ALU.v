095d028154720837ed13fe843065c3890ee1d9cf
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-19 13:48:28 +0200

    vault backup: 2024-04-19 13:48:28 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab05/src/ALU.v
    src/sem2/DDCA/lab/lab05/src/Logic.v
    src/sem2/DDCA/lab/lab05/src/Mux.v
    src/sem2/DDCA/var/intro/mux2.v
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab05/src/ALU.v
+++ src/sem2/DDCA/lab/lab05/src/ALU.v
@@ -1,29 +1,38 @@
 module ALU (
-    input [3:0] OP,
+    input [3:0] op,
     input [31:0] A, B,
     output reg [31:0] Y,
     output reg Z
     );
-    
-    parameter ADD = 4'b0000;
-    parameter SUB = 4'b0010;
-    parameter AND = 4'b0100;
-    parameter OR  = 4'b0101;
-    parameter XOR = 4'b0110;
-    parameter NOR = 4'b0111;
-    parameter SLT = 4'b1010;
 
-    always @ (*) begin
-        case (OP)
-            ADD: Y <= A + B;
-            SUB: Y <= A - B;
-            AND: Y <= A & B;
-            OR:  Y <= A | B;
-            XOR: Y <= A ^ B;
-            NOR: Y <= ~(A | B);
-            SLT: Y <= (A - B < 0) ? 32'b1 : 32'b0;
-            default: Y <= 32'b0;
-        endcase
-        Z <= |Y;
-    end
+
+
+
+
+
+
+
+
+
+
+    // parameter ADD = 4'b0000;
+    // parameter SUB = 4'b0010;
+    // parameter AND = 4'b0100;
+    // parameter OR  = 4'b0101;
+    // parameter XOR = 4'b0110;
+    // parameter NOR = 4'b0111;
+    // parameter SLT = 4'b1010;
+    // always @ (*) begin
+    //     case (op)
+    //         ADD: Y <= A + B;
+    //         SUB: Y <= A - B;
+    //         AND: Y <= A & B;
+    //         OR:  Y <= A | B;
+    //         XOR: Y <= A ^ B;
+    //         NOR: Y <= ~(A | B);
+    //         SLT: Y <= (A - B < 0) ? 32'b1 : 32'b0;
+    //         default: Y <= 32'b0;
+    //     endcase
+    //     Z <= |Y;
+    // end
 endmodule
---
+++ src/sem2/DDCA/lab/lab05/src/Logic.v
@@ -0,0 +1,7 @@
+module Logic (
+    input [1:0] op,
+    input [31:0] A, B,
+    output [31:0] Y
+    );
+        
+endmodule
