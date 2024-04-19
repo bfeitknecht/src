384c07bd32a961ef20ed850405c2379c9a65d8f4
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-19 14:55:44 +0200

    vault backup: 2024-04-19 14:55:44 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab05/src/Adder.v

--- src/sem2/DDCA/lab/lab05/src/Adder.v
+++ src/sem2/DDCA/lab/lab05/src/Adder.v
@@ -1,14 +1,41 @@
-module FullAdder (input a, input b, input ci, output s, output co);
+module FullAdder (input a, input b, input ci, output so, output co);
     
     wire so0, co0, co1;
 
     xor s0 (so0, a, b);
-    xor SUM (s, s0, ci);
+    xor SUM (so, s0, ci);
     
     and c0 (co0, a, b);
     and c1 (co1, s0, ci);
     or CARRY (co, c0, c1);
 
+endmodule
+
+// module FourBitAdder (
+//     input [3:0] a,
+//     input [3:0] b,
+//     output wire [4:0] S);
+    
+//     wire [2:0] c;
+
+//     FullAdder FA0(.a(a[0]), .b(b[0]), .ci(), .so(S[0]), .co(c[0]));
+//     FullAdder FA1(.a(a[1]), .b(b[1]), .ci(c[0]), .so(S[1]), .co(c[1]));
+//     FullAdder FA2(.a(a[2]), .b(b[2]), .ci(c[1]), .so(S[2]), .co(c[2]));
+//     FullAdder FA3(.a(a[3]), .b(b[3]), .ci(c[2]), .so(S[3]), .co(S[4]));
+// endmodule
+
+
+module Adder16 (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] S);
+    
+    wire [2:0] c;
+
+    FullAdder FA0(.a(a[0]), .b(b[0]), .ci(), .so(S[0]), .co(c[0]));
+    FullAdder FA1(.a(a[1]), .b(b[1]), .ci(c[0]), .so(S[1]), .co(c[1]));
+    FullAdder FA2(.a(a[2]), .b(b[2]), .ci(c[1]), .so(S[2]), .co(c[2]));
+    FullAdder FA3(.a(a[3]), .b(b[3]), .ci(c[2]), .so(S[3]), .co(S[4]));
 endmodule
 
 module Adder (
