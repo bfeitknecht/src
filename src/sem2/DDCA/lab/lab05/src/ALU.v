fa7a41dfe551868a21de78e41d41e431a7f9179a
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-19 14:20:44 +0200

    vault backup: 2024-04-19 14:20:43 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab05/src/ALU.v
    src/sem2/DDCA/lab/lab05/src/Logic.v

--- src/sem2/DDCA/lab/lab05/src/ALU.v
+++ src/sem2/DDCA/lab/lab05/src/ALU.v
@@ -1,22 +1,70 @@
+module Mux #(parameter data = 8) (
+    input s,
+    input [data-1:0] A, B,
+    output [data-1:0] Y);
+
+    assign Y = s ? B : A;
+endmodule
+
 module Arithmetic (
-    input [1:0] op,
+    input op,
     input [31:0] A, B,
-    output reg [31:0] Y
+    output [31:0] Y
     );
 
-    always @ (*) begin
-        case (op)
-            2'b00: Y <= A + B;
-            2'b10: Y <= A - B;
-        endcase
-    end
+    wire [31:0] add, sub;
+
+    assign add = A + B; 
+    assign sub = A - B; 
+
+    Mux #(.data(32)) operation (
+        .s(op),
+        .A(add),
+        .B(sub),
+        .Y(Y)
+    );
+endmodule
+
+module Logic (
+    input [1:0] op,
+    input [31:0] A, B,
+    output [31:0] Y
+    );
+    
+    wire [31:0] AND, OR, XOR, NOR, ANDOR, XORNOR;
+
+    assign AND = A & B;
+    assign OR = A | B;
+    assign XOR = A ^ B;
+    assign NOR = A ~| B;
+
+    Mux #(.data(32)) andor (
+        .s(op[0]),
+        .A(AND),
+        .B(OR),
+        .Y(ANDOR)
+    );
+
+    Mux #(.data(32)) xornor (
+        .s(op[0]),
+        .A(XOR),
+        .B(NOR),
+        .Y(XORNOR)
+    );
+    
+    Mux #(.data(32)) operation (
+        .s(op[1]),
+        .A(ANDOR),
+        .B(XORNOR),
+        .Y(Y)
+    );
 endmodule
 
 module ALU (
-    input op,
+    input [3:0] op,
     input [31:0] A, B,
-    output reg [31:0] Y,
-    output reg Z
+    output [31:0] Y,
+    output Z
     );
 
     wire [31:0] out_AL;
--- src/sem2/DDCA/lab/lab05/src/Logic.v
+++ src/sem2/DDCA/lab/lab05/src/Logic.v
@@ -1,7 +1,7 @@
-module Mux #(parameter width = 8) (
+module Mux #(parameter data = 8) (
     input s,
-    input [width-1:0] A, B,
-    output [width-1:0] Y);
+    input [data-1:0] A, B,
+    output [data-1:0] Y);
 
     assign Y = s ? B : A;
 endmodule
@@ -9,7 +9,7 @@ endmodule
 module Logic (
     input [1:0] op,
     input [31:0] A, B,
-    output reg [31:0] Y
+    output [31:0] Y
     );
     
     wire [31:0] AND, OR, XOR, NOR, ANDOR, XORNOR;
@@ -19,35 +19,24 @@ module Logic (
     assign XOR = A ^ B;
     assign NOR = A ~| B;
 
-    Mux #(.width(32)) andor (
-        .s(op[1]),
+    Mux #(.data(32)) andor (
+        .s(op[0]),
         .A(AND),
         .B(OR),
         .Y(ANDOR)
     );
 
-    Mux #(.width(32)) xornor (
-        .s(op[1]),
+    Mux #(.data(32)) xornor (
+        .s(op[0]),
         .A(XOR),
         .B(NOR),
         .Y(XORNOR)
     );
-
-    Mux #(.width(32)) operation (
+    
+    Mux #(.data(32)) operation (
         .s(op[1]),
         .A(ANDOR),
         .B(XORNOR),
         .Y(Y)
     );
-
-
-
-    always @ (*) begin
-        case (op)
-            2'b00: Y <= A & B;
-            2'b01: Y <= A | B;
-            2'b10: Y <= A ^ B;
-            2'b11: Y <= A ~| B;
-        endcase
-    end
 endmodule
