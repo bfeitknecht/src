b410db8d09c37d302fc07a4d509497312618555a
bfeitknecht <bfeitknecht@ethz.ch>
2024-04-20 17:57:01 +0200

    vault backup: 2024-04-20 17:57:01 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab04/src.0.1/FSM.v

--- src/sem2/DDCA/lab/lab04/src.0.1/FSM.v
+++ src/sem2/DDCA/lab/lab04/src.0.1/FSM.v
@@ -1,13 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module ClockDivisor #(parameter ratio = 1) (
+    input reset,
+    input clk_in,
+    output clk_out
+    );
+    
+    reg [ratio-1:0] count;
+    always @ (posedge clk_in) begin
+        if (reset) count <= 0;
+        else count <= count + 1;
+    end
+    assign clk_out = &count;
+endmodule
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 module FSM (
-    input clk,
+    input clk_sys,
     input reset,
     input [1:0] enable,
     output [5:0] LED,
     output A, B, C
     );
 
-    reg [2:0] state_prev, state_next;
 
+    ClockDivisor (.ratio(25)) clk_div (
+        .reset(reset),
+        .clk_in(clk_sys),
+        .clk_out(clk)
+        );
+
+    // state holding register
+    reg [5:0] state_prev, state_next;
+    always @ (posedge clk, posedge reset) begin
+        if (reset) state_prev <= S00;
+        else state_prev <= state_next;
+    end
+    
+    // next state logic 
     parameter S00 = 6'b000000;
     parameter S10 = 6'b001000;
     parameter S20 = 6'b011000;
@@ -16,30 +48,31 @@ module FSM (
     parameter S02 = 6'b000110;
     parameter S03 = 6'b000111;
     parameter S11 = 6'b111111;
-
-    
-    // state holding register
-    always @ (posedge clk, posedge reset) begin
-        if (reset) state_prev <= S00;
-        else state_prev <= state_next;
-    end
-    
-    // next state logic 
     always @ (posedge clk) begin
         case (state_prev)
             S00: begin
                 if (enable[1]) state_next <= S10;
-                else if (enable[0]) state_next <= S01;
-                else state_next <= S00;
+                if (enable[0]) state_next <= S01;
+                if (&enable) state_next <= S11;
             end 
+
             S10: state_next <= S20;
             S20: state_next <= S30;
             S30: state_next <= S00;
+
+            S01: state_next <= S02;
+            S02: state_next <= S03;
+            S03: state_next <= S00;
         endcase
     end
 
+    always @ (posedge clk_sys) begin
+
+
+    end
+
     // output logic
-    assign {C, B, A} = state_prev;
+    assign LED = state_prev;
 endmodule
 
 
