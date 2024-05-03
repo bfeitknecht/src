c0de344694f3d0544043a23eff0353ac80450912
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-03 11:19:13 +0200

    vault backup: 2024-05-03 11:19:13 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/lab7.asm
    vault/.obsidian/workspace.json

--- src/sem2/DDCA/lab/lab07/lab7.asm
+++ src/sem2/DDCA/lab/lab07/lab7.asm
@@ -5,24 +5,23 @@
 #	Basil Feitknecht, Camil Schmidt
 #
 # use only:
-# ADD, SUB, SLT, XOR, AND, OR, NOR, J, ADDI, BEQ.
+# 	ADD, SUB, SLT, XOR, AND, OR, NOR, J, ADDI, BEQ.
 
 
 
 .text
 main:
-	addi $s0, $0, 0	# initialize counter at A
-
-	j loop	# Jump to loop 
-
-	# Put your sum S into register $t2
-end:	
-	j	end	# Infinite loop at the end of the program. 
-
+	addi $s0, $0, 5		# A = 5
+	addi $s1, $0, 10	# B = 10
+	addi $t2, $0, 0		# S = 0
+	j loop				# Jump to loop 
 
 loop:
 	beq $s0, $s1, end	# If A == B, jump to end
+	add $t2, $t2, $s0	# S = S + A
+	addi $s0, $s0, 1 	# A = A + 1
+	j loop				# jump to loop
 
 
-.data
-oiuahdiua: .asciiz "Hello World\n"
+end:	
+	j	end				# Infinite loop at the end of the program. 
