c6f3bc6bdde41b729b674496103d9fa8dc5751dd
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-09 16:12:10 +0200

    vault backup: 2024-05-09 16:12:10 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab06/report/UUT.v

--- src/sem2/DDCA/lab/lab06/report/UUT.v
+++ src/sem2/DDCA/lab/lab06/report/UUT.v
@@ -1,3 +1,50 @@
+module FSM (
+    input clk,
+    input reset,
+    input eLeft,
+    input eRight,
+    output [2:0] stateL, stateR
+    );
+
+    // state holding registers
+    reg [2:0] stateL_prev, stateL_next, stateR_prev, stateR_next;
+    parameter S0 = 3'b000;
+    parameter S1 = 3'b001;
+    parameter S2 = 3'b011;
+    parameter S3 = 3'b111;
+
+    // update state based on inputs 
+    always @ (*) begin
+        case (stateL_prev)
+            S0: stateL_next <= (eLeft && stateR_prev[0] == 0) ? S1 : S0;
+            S1: stateL_next <= S2;
+            S2: stateL_next <= S3;
+            S3: stateL_next <= S0;
+        endcase
+        case (stateR_prev)
+            S0: stateR_next <= (eRight && stateL_prev[0] == 0) ? S1 : S0;
+            S1: stateR_next <= S2;
+            S2: stateR_next <= S3;
+            S3: stateR_next <= S0;
+        endcase
+    end
+
+    // next state logic
+    always @ (posedge clk, posedge reset) begin
+        if (reset) begin
+            stateL_prev <= S0;
+            stateR_prev <= S0;
+        end else begin 
+            stateL_prev <= stateL_next;
+            stateR_prev <= stateR_next;
+        end
+    end
+
+    // output logic
+    assign stateL = stateL_prev;
+    assign stateR = stateR_prev;
+endmodule
+
 module BlinkingLightsTest();
     reg reset;
     reg left;
@@ -61,10 +108,10 @@ module BlinkingLightsTest();
     end
     FSM uut(
         .reset(reset),
-        .left(left),
-        .right(right),
-        .clock(clk),
-        .lights_right(lights[0]),
-        .lights_left(lights[1])
+        .eLeft(left),
+        .eRight(right),
+        .clk(clk),
+        .stateR(lights[0]),
+        .stateL(lights[1])
     );
 endmodule
