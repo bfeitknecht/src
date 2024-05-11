a5563185c3ea62cb7745d55f14cfaab93b06bb6e
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-11 22:53:45 +0200

    vault backup: 2024-05-11 22:53:45 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/lab/sad.asm

--- src/sem2/DDCA/lab/lab07/lab/sad.asm
+++ src/sem2/DDCA/lab/lab07/lab/sad.asm
@@ -7,7 +7,7 @@
 
 .data   
 
-n:   .word   9
+n:      .word   9
 A:      .word   5, 16, 7, 1, 1, 13, 2, 8, 10
 B:      .word   4, 15, 8, 0, 2, 12, 3, 7, 11
 SAD:    .space  36
@@ -16,118 +16,93 @@ SAD:    .space  36
 
 main:   
 
-    la      $s0,            A                   # Load the address of A into $s0
-    li      $s1,            0                   # $s1 = i = 0
-    sw      $s2,            n                # $s2 = n
-    la      $s3,            SAD                 # Load the address of SAD into $s3
+    la      $s0,            A                       # Load the address of A into $s0
+    li      $s1,            0                       # $s1 = i = 0
+    sw      $s2,            n                       # $s2 = n
+    la      $s3,            SAD                     # Load the address of SAD into $s3
 
 
     # Loop over the elements of left_image and right_image
-    li      $s1,            0                   # $s1 = i = 0
-    move    $s2,            size                # $s2 = size
+    li      $s1,            0                       # $s1 = i = 0
+    move    $s2,            size                    # $s2 = size
     j       loop
 
 
 loop:   
-    bgt     $s1,            $s2,        break   # If i == image_size, jump to break
+    bgt     $s1,            $s2,        break       # If i == image_size, jump to break
 
-    lw      $a0,            0($s0)              # Load left_image[i] into $a0
-    lw      $a1,            36($s0)             # Load right_image[i] into $a1
-    jal     abs_diff                            # Call abs_diff
-    sw      $v0,            72($s0)             # Store the result of abs_diff in sad_array[i]
+    lw      $a0,            0($s0)                  # Load left_image[i] into $a0
+    lw      $a1,            36($s0)                 # Load right_image[i] into $a1
+    jal     abs_diff                                # Call abs_diff
+    sw      $v0,            72($s0)                 # Store the result of abs_diff in sad_array[i]
 
-    addi    $s1,            $s1,        1       # i++
-    addi    $s0,            $s0,        4       # Move to the next element in the array
-    j       loop                                # repeat loop
+    addi    $s1,            $s1,        1           # i++
+    addi    $s0,            $s0,        4           # Move to the next element in the array
+    j       loop                                    # repeat loop
 
 
 break:  
     # recursive_sum(sad_array, 9)
-    move    $s0,            $0                  # $s0 = 0, move back to the beginning of the array
-    lw      $a0,            72($s0)             # Load the base address of sad_array into $a0
-    addi    $a1,            $0,         9       # Load the size of the array into $a1
-    jal     recursive_sum                       # call recursive_sum
+    move    $s0,            $0                      # $s0 = 0, move back to the beginning of the array
+    lw      $a0,            72($s0)                 # Load the base address of sad_array into $a0
+    addi    $a1,            $0,         9           # Load the size of the array into $a1
+    jal     recursive_sum                           # call recursive_sum
 
-    sw      $v0,            0($t2)              # Store the returned value in $t2
+    sw      $v0,            0($t2)                  # Store the returned value in $t2
 
     j       END
 
 
 END:    
-    j       END                                 # Infinite loop at the end of the program.
+    j       END                                     # Infinite loop at the end of the program.
 
 
 
     # Absolute difference between two integers
 abs_diff:
-    sub     $t1,            $a0,        $a1     # Subtract second integer from first integer
-    sra     $t2,            $t1,        31      # Arithmetic right shift to get sign of the difference
-    xor     $t1,            $t1,        $t2     # XOR to flip the sign if necessary
-    sub     $v0,            $t1,        $t2     # Subtract the flipped sign from the difference to get absolute difference
-    jr      $ra                                 # Return to the calling function
+    sub     $t1,            $a0,        $a1         # Subtract second integer from first integer
+    sra     $t2,            $t1,        31          # Arithmetic right shift to get sign of the difference
+    xor     $t1,            $t1,        $t2         # XOR to flip the sign if necessary
+    sub     $v0,            $t1,        $t2         # Subtract the flipped sign from the difference to get absolute difference
+    jr      $ra                                     # Return to the calling function
 
-    # Recursive sum of an array of integers
-recursive_sum:
-    addi    $sp,            $sp,        -8      # Adjust sp
-    addi    $t0,            $a1,        -1      # Compute size - 1
-    sw      $t0,            0($sp)              # Save size - 1 to stack
-    sw      $ra,            4($sp)              # Save return address
-    bne     $a1,            $zero,      else    # size == 0 ?
-    addi    $v0,            $0,         0       # If size == 0, set return value to 0
-    addi    $sp,            $sp,        8       # Adjust sp
-    jr      $ra                                 # Return
+    # MIPS assembly code for summing elements of an integer array
 
-else:   
-    add     $a1,            $t0,        $0      # update the second argument
-    jal     recursive_sum
-    lw      $t0,            0($sp)              # Restore size - 1 from stack
-    sll     $t1,            $t0,        2       # Multiply size by 4
-    add     $t1,            $t1,        $a0     # Compute & arr[ size - 1 ]
-    lw      $t2,            0($t1)              # t2 = arr[ size - 1 ]
-    add     $v0,            $v0,        $t2     # retval = $v0 + arr[size - 1]
-    lw      $ra,            4($sp)              # restore return address from stack
-    addi    $sp,            $sp,        8       # Adjust sp
-    jr      $ra                                 # Return
+    # Registers:
+    # $a0: pointer to the array A
+    # $a1: n, the number of elements in the array
+    # $t0: loop counter
+    # $t1: temporary variable for storing the sum
+    # $t2: pointer to the current element of the array
 
-
-    # Sum of an array of integers
 sum:    
-    # $a0 = base address of the array
-    # $a1 = size of the array
-
-    lw      $t0,            0($s0)
-    add     $t1,            $t1,        $t0
-    addi    $s0,            4
-    addi    $a1,            1
-
-    bne     $s1,            $a1,        sum     # if i != size, repeat the loop
-    jr      $ra                                 # return to the calling function
-
-
-
-sum_array:
     # Initialize sum to 0
-    li $v0, 0            # $v0 = sum
+    li      $t1,            0                       # $t1 = 0
 
     # Loop through the array
-    li $t0, 0            # $t0 = i (loop counter)
-    loop:
-        # Check if we have reached the end of the array
-        bge $t0, $a1, end_loop   # If i >= size, exit loop
+    li      $t0,            0                       # $t0 = 0 (loop counter)
+loop_start:
+    # Check if we have reached the end of the array
+    bge     $t0,            $a1,        loop_end    # If $t0 >= $a1, exit loop
 
-        # Load the current element of the array into $t1
-        lw $t1, 0($a0)     # $t1 = array[i]
+    # Load the current element of the array into $t2
+    mul     $t2,            $t0,        4           # $t2 = $t0 * 4 (each integer is 4 bytes)
+    add     $t2,            $t2,        $a0         # $t2 = address of A[i]
+    lw      $t2,            0($t2)                  # $t2 = A[i]
 
-        # Add the current element to the sum
-        add $v0, $v0, $t1  # sum += array[i]
+    # Add the current element to the sum
+    add     $t1,            $t1,        $t2         # $t1 = $t1 + A[i]
 
-        # Move to the next element of the array
-        addi $a0, $a0, 4   # Increment the base address by 4 bytes (size of integer)
-        addi $t0, $t0, 1   # Increment loop counter
+    # Increment the loop counter
+    addi    $t0,            $t0,        1           # $t0++
 
-        # Repeat the loop
-        j loop
+    # Repeat the loop
+    j       loop_start
 
-    end_loop:
+    # End of loop
+loop_end:
+
     # Return the sum
-    jr $ra
+    move    $v0,            $t1                     # Return value in $v0
+    jr      $ra                                     # Return
+
