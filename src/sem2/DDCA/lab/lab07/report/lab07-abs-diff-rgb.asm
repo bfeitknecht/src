7fdc1e0c124aca21ba3d6acdae061818f2bfd4f4
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-15 16:57:58 +0200

    vault backup: 2024-05-15 16:57:58 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm

--- src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm
+++ src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm
@@ -9,32 +9,40 @@
 
 main:
     # Initialize the arrays
-    li      $s0,        0
-    li      $t0,        1                   # R1
-    li      $t1,        2                   # R2
-    li      $t2,        3                   # G1
-    li      $t3,        4                   # G2
-    li      $t4,        0                   # B1
-    li      $t5,        4                   # B2
+    li      $s0,            0
+    li      $t0,            1           # R1
+    li      $t1,            2           # R2
+    li      $t2,            3           # G1
+    li      $t3,            4           # G2
+    li      $t4,            0           # B1
+    li      $t5,            4           # B2
 
     # Prepare the function arguments
-    move    $a0,        $t0
-    move    $a1,        $t1
-    move    $a2,        $t2
-    move    $a3,        $t3
+    move    $a0,            $t0
+    move    $a1,            $t1
+    move    $a2,            $t2
+    move    $a3,            $t3
 
-end:
-    j       end                             # Infinite loop at the end of the program.
+    jal     abs_diff_color
 
 
-    # Absolute difference between two integers
-    # $a0 = l
-    # $a1 = r
+    # Absolute difference between three channels
+abs_diff_color:
+    #       
+    #       
+    #       
+
+
+    # Absolute difference, $a0 = l, $a1 = r
 abs_diff:
-    sub     $t1,        $a0,        $a1     # Subtract second integer from first integer
-    sra     $t2,        $t1,        31      # Arithmetic right shift to get sign of the difference
-    xor     $t1,        $t1,        $t2     # XOR to flip the sign if necessary
-    sub     $v0,        $t1,        $t2     # Subtract flipped sign from difference to get absolute difference
-    jr      $ra                             # Return to the calling function
+    sub     $t1,            $a0,    $a1 # Subtract second integer from first integer
+    sra     $t2,            $t1,    31  # Arithmetic right shift to get sign of the difference
+    xor     $t1,            $t1,    $t2 # XOR to flip the sign if necessary
+    sub     $v0,            $t1,    $t2 # Subtract flipped sign from difference to get absolute difference
+    jr      $ra                         # Return to the calling function
 
 
+
+    # Infinite loop at the end of the program
+end:
+    j       end
