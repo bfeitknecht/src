6ff2d7686f1d071c75c66e4b0bc4d6128ffc419f
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-16 15:45:07 +0200

    vault backup: 2024-05-16 15:45:07 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm

--- src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm
+++ src/sem2/DDCA/lab/lab07/report/lab07-abs-diff-rgb.asm
@@ -9,23 +9,20 @@
 
 main:
     li      $s0,            0           # Initialize the counter
-    li      $t0,            1           # R1
-    li      $t1,            2           # R2
-    li      $t2,            3           # G1
-    li      $t3,            4           # G2
-    li      $t4,            0           # B1
-    li      $t5,            4           # B2
 
     # Prepare the function arguments
-    move    $a0,            $t0
-    move    $a1,            $t1
-    move    $a2,            $t2
-    move    $a3,            $t3
+    li      $a0,            1           # R1
+    li      $a1,            2           # R2
+    li      $a2,            3           # G1
+    li      $a3,            4           # G2
 
     sub     $sp,            $sp,    8   # Reserve space on the stack
-    sw      $t4,            0($sp)      # Save $t4 on the stack
-    sw      $t5,            4($sp)      # Save $t5 on the stack
-    add     $sp,            $sp,    8   # Restore the stack pointer
+
+    li      $sp,            5           # Save $a4 on the stack, B1
+    add     $sp,            $sp,    4   # go to next stack argument
+
+    li      $sp,            6           # Save $a5 on the stack, B2
+    add     $sp,            $sp,    4   # go to next stack argument
 
     jal     abs_diff_color
     move    $t9,            $v0         # Save the result in $t9
@@ -35,20 +32,20 @@ main:
     # Absolute difference between three channels
 abs_diff_color:
     jal     abs_diff                    # abs_diff(R1, R2)
-    add     $t0,            $t0,    $v0 # Add the result to $t0
+    add     $s0,            $t0,    $v0 # Add the result to $t0
 
     move    $a0,            $a2         # Prepare the function arguments
     move    $a1,            $a3
     jal     abs_diff                    # abs_diff(G1, G2)
-    add     $t0,            $t0,    $v0 # Add the result to $t0
+    add     $s0,            $t0,    $v0 # Add the result to $t0
 
     sub     $sp,            $sp,    8   # Get the stack stored values
     sw      $a0,            0($sp)      # Prepare the function arguments
     sw      $a1,            4($sp)
     jal     abs_diff                    # abs_diff(B1, B2)
-    add     $t0,            $t0,    $v0 # Add the result to $t0
+    add     $s0,            $t0,    $v0 # Add the result to $t0
 
-    move    $v0,            $t0         # Return the result in $v0
+    move    $v0,            $s0         # Return the result in $v0
 
     # jr      $ra                         # Return to the calling function
     # j       end
