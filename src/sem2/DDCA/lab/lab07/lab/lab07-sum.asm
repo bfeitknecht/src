fc9751fb7e55b266fd622290ccafc35738b99c6d
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-17 10:58:03 +0200

    vault backup: 2024-05-17 10:58:03 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm

--- src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm
+++ src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm
@@ -11,14 +11,12 @@
 
 .text
 main:
-    addi    $s0,    $0,     12  # A = 5
-    addi    $s1,    $0,     10  # B = 32767
+    addi    $s0,    $0,     12  # A = 12
+    addi    $s1,    $0,     10  # B = 10
     addi    $s1,    $s1,    1   # B = B + 1
     addi    $t2,    $0,     0   # S = 0
-    
-    slt     $t1,    $s0,    $s1 # B < A
-    beq     $t1,    $0,     end # If A < B, jump to loop
-
+    slt     $t1,    $s0,    $s1 # $t1 = A < B ? 1 : 0
+    beq     $t1,    $0,     end # If A > B, jump to end
     j       loop                # Jump to loop
 
 loop:
