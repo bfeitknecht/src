09fa6cca9060379d54ef537397edd2726e980d20
bfeitknecht <bfeitknecht@ethz.ch>
2024-05-17 10:47:04 +0200

    vault backup: 2024-05-17 10:47:04 from MacBook Pro (echo)
    Affected files:
    src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm

--- src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm
+++ src/sem2/DDCA/lab/lab07/lab/lab07-sum.asm
@@ -11,20 +11,20 @@
 
 .text
 main:
-    addi    $s0,    $0,     12       # A = 5
-    addi    $s1,    $0,     10   # B = 32767
-    addi    $s1,    $s1,    1       # B = B + 1
-    addi    $t2,    $0,     0       # S = 0
-    blt	    $s1,    $s0,    end
-    j       loop                    # Jump to loop
-
+    addi    $s0,    $0,     12  # A = 5
+    addi    $s1,    $0,     10  # B = 32767
+    addi    $s1,    $s1,    1   # B = B + 1
+    addi    $t2,    $0,     0   # S = 0
+    slt     $t1,    $s1,    $s0 # A < B
+    beq     $t1,    $0,     end # If B < A, jump to loop
+    j       loop                # Jump to loop
 
 loop:
-    beq     $s0,    $s1,    end     # If A == B, jump to end
-    add     $t2,    $t2,    $s0     # S = S + A
-    addi    $s0,    $s0,    1       # A = A + 1
-    j       loop                    # jump to loop
+    beq     $s0,    $s1,    end # If A == B, jump to end
+    add     $t2,    $t2,    $s0 # S = S + A
+    addi    $s0,    $s0,    1   # A = A + 1
+    j       loop                # jump to loop
 
 
 end:
-    j       end                     # Infinite loop at the end of the program.
+    j       end                 # Infinite loop at the end of the program.
