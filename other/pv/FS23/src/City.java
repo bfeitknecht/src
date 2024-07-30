b73e020dbb89462fc0f10e017868a9341a8543b3
bfeitknecht <bfeitknecht@ethz.ch>
2024-07-30 16:12:32 +0200

    bump: PProg
    modified:   other/pv/FS23/src/City.java
    modified:   sem2/PProg/pprog-bfeitknecht

--- other/pv/FS23/src/City.java
+++ other/pv/FS23/src/City.java
@@ -1,22 +1,18 @@
-
-
-
 import java.util.Set;
 
 public class City {
 
-	public int size;
+    public int size;
 
-	public Set<City> oneWayStreets;
+    public Set<City> oneWayStreets;
 
-	public City(int size, Set<City> next) {
-		this.size = size;
-		this.oneWayStreets = next;
-	}
-	
-	@Override
-	public String toString() {
-		return super.toString()+" <City size=" + this.size + ">";
-	}
+    public City(int size, Set<City> next) {
+        this.size = size;
+        this.oneWayStreets = next;
+    }
 
+    @Override
+    public String toString() {
+        return super.toString() + " <City size=" + this.size + ">";
+    }
 }
